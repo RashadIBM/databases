f4562088bfa726210849138b0bf5bab5b00815f2
Brady <brady.y.liu@gmail.com>
2019-03-20 20:24:37 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,25 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE messages(
+  MessagesID INT not null,
+  Messages VARCHAR(255) NOT NULL,
+  Lobby VARCHAR(20),
+  primary key (MessagesID)
 );
 
-/* Create other tables and define schemas for them here! */
+-- DESCRIBE messages;
+CREATE TABLE users(
+  UserID INT not null,
+  UserName VARCHAR(16) NOT NULL,
+  FavoriteLobby VARCHAR(20),
+  primary key (UserID)
+);
 
-
-
-
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+-- /*  Execute this file from the command line by typing:
+--  *    mysql -u root < server/schema.sql
+--  *  to create the database and the tables.*/
 
